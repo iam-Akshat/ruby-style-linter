390d51c27d0c2ad555d8219a7c8388634024c9f2
Akshat Sethi <masterakshata@gmail.com>
2020-12-08 16:18:53 +0530

    added time consumed and file processed information

--- lib/linter.rb
+++ lib/linter.rb
@@ -9,8 +9,11 @@ class Linter
   end
 
   def lint
+    starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     @file_store.each do |file_path, _file_name|
       Indentation.lint_indentation(file_path, file_path.gsub(Dir.getwd, ''))
     end
+    ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    puts "Processed #{@file_store.length} files in #{ending-starting} seconds"
   end
 end
