ebcdd2f5d36134cbf7fbf45b9f8652b8ddb2abb9
Akshat Sethi <masterakshata@gmail.com>
2020-12-08 19:00:26 +0530

    moved dir_test_folder out of rspec

--- spec/linters_helpers_spec.rb
+++ spec/linters_helpers_spec.rb
@@ -4,7 +4,7 @@ describe 'LinterHelpers' do
   describe '#traverse_dir' do
     it 'takes starting path,a hash to store files with path, passing the filter which is an extension' do
       include LinterHelpers
-      test_dir = "#{Dir.getwd}/spec/dir_test_folder/"
+      test_dir = "#{Dir.getwd}/dir_test_folder/"
       file_store = {}
       LinterHelpers.traverse_dir(test_dir, file_store)
       expect(file_store).to include(
